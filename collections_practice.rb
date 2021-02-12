c1db1b85b228cdc32bc550a1f83652c795abdb8a
Michael Dobak <mdobak44@icloud.com>
2021-02-12 18:19:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort 
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse!
+end   
+
+def sort_array_char_count(array)
+  array.sort_by{|x| x.length}
+end   
+
+def swap_elements(array) 
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+
+def reverse_array(array)
+  array.reverse!
+end     
+
+def kesha_maker(array)
+  array.map do |n|
+    n.slice!(2)
+    n.insert(2, "$")
+  end
+end   
+
+def find_a(array)
+  array.select {|a| a.start_with?("a")}
+end  
+
+def sum_array(array)
+  array.inject(0) {|a, b| a + b}
+end  
+
+def add_s(array)
+  
+end    
+      
+  
