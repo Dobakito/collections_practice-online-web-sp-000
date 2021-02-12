37f17fb8f09cb5ef4f3a8f3fdc4405a6d0170daa
Michael Dobak <mdobak44@icloud.com>
2021-02-12 18:21:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,9 +35,7 @@ def sum_array(array)
 end  
 
 def add_s(array)
-  array.each_with_index.reject {|a, i| i == 1}.each do |a ,i|
-    puts "#{a}s"
-  end  
+  array.collect {|a| puts "#{a}s"
 end    
       
   
